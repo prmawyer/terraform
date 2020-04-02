c84e2bd06a817e1a549711f1923ad5808a624d3d
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-04-02 14:57:52 -0400

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
    backend "s3" {
-   bucket = "pm-tf-remote-state-bucket"
+   bucket = "pm-tf-bucket-training-exercise-1"
    key = "terraform.tfstate"
    region = "us-east-1"
   }
