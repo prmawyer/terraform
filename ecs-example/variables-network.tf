26606da8e07b0429e62e4be1ae6115567cf67c08
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-07-14 15:33:36 -0400

    Update variables-network.tf

--- ecs-example/variables-network.tf
+++ ecs-example/variables-network.tf
@@ -1,12 +1,1 @@
-# ECS cluster variables | variables-network.tf
-variable "app_name" {
-  type = string
-  description = "App being deployed to ECS"
-  default = "t2.small"
-}
 
-variable "app_environment" {
-  type = string
-  description = "Environment to which app is deployed"
-  default = "Harness-SE-testing"
-}
