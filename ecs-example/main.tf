d5ca41249229ee88bd5065c798b1caeb4cf9bf32
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-07-14 15:38:15 -0400

    Update main.tf

--- ecs-example/main.tf
+++ ecs-example/main.tf
@@ -1,8 +1,10 @@
-backend "s3" {
-   bucket = "patrick-harness"
-   key = "terraform.tfstate"
-   region = "us-east-1"
-  }
+Terraform {
+   backend "s3" {
+      bucket = "patrick-harness"
+      key = "terraform.tfstate"
+      region = "us-east-1"
+   }
+}
 
 provider "aws" {
    region = var.aws_region
