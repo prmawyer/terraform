bd831c5899e2f0242dc7593ac56bbf6cd5357ac8
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-07-14 15:33:56 -0400

    Update variables.tf

--- ecs-example/variables.tf
+++ ecs-example/variables.tf
@@ -19,3 +19,16 @@ variable "aws_region" {
   type = string
   description = "AWS region"
 }
+
+# ECS cluster variables | variables-network.tf
+variable "app_name" {
+  type = string
+  description = "App being deployed to ECS"
+  default = "t2.small"
+}
+
+variable "app_environment" {
+  type = string
+  description = "Environment to which app is deployed"
+  default = "Harness-SE-testing"
+}
