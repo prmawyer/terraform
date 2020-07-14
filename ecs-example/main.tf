5c5d9d868c2cc892c04ac36506517418631d58aa
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-07-14 15:36:28 -0400

    Update main.tf

--- ecs-example/main.tf
+++ ecs-example/main.tf
@@ -1,6 +1,11 @@
-terraform {
-   backend "s3" {
+backend "s3" {
    bucket = "patrick-harness"
    key = "terraform.tfstate"
    region = "us-east-1"
   }
+
+provider "aws" {
+   region = var.aws_region
+   access_key = var.aws_access_key
+   secret_key = var.aws_secret_key
+}
