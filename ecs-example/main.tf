8fa508e1b5ffa0bbd489a9ed1606af40f3f76d02
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-07-15 07:19:14 -0400

    Update main.tf

--- ecs-example/main.tf
+++ ecs-example/main.tf
@@ -1,15 +1,15 @@
 terraform {
    backend "s3" {
-      bucket = "patrick-harness"
-      key = "terraform.tfstate"
-      region = "us-east-1"
+      bucket = ${var.bucketname}
+      key = ${var.statefile}
+      region = ${var.region}
    }
 }
 
 provider "aws" {
-   region = var.aws_region
-   access_key = var.aws_access_key
-   secret_key = var.aws_secret_key
+   region = ${var.region}
+   access_key = ${var.aws_access_key}
+   secret_key = ${var.aws_secret_key}
 }
 
 # Network Setup: VPC, Subnet, IGW, Routes | network.tf
