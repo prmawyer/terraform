021ac3b5a6a58d68f46b24722264bef5c90ffe22
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-07-15 08:18:19 -0400

    Update main.tf

--- ecs-example/main.tf
+++ ecs-example/main.tf
@@ -7,9 +7,9 @@ terraform {
 }
 
 provider "aws" {
-   region = "${var.region}"
-   access_key = "${var.aws_access_key}"
-   secret_key = "${var.aws_secret_key}"
+   region = "var.region"
+   access_key = "var.aws_access_key"
+   secret_key = "var.aws_secret_key"
 }
 
 # Network Setup: VPC, Subnet, IGW, Routes | network.tf
