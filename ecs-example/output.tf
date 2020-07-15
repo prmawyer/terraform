bd3684780c3f29b441a908513235674990ef0a25
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-07-15 07:32:34 -0400

    Update output.tf

--- ecs-example/output.tf
+++ ecs-example/output.tf
@@ -9,7 +9,7 @@ output "ecs_cluster_name" {
   }
 
 output "target_execution_role" {
-  value = var.app_name
+  value = aws_iam_role_policy.ecs-cluster-runner-role-policy.name
   }
 
 output "region" {
@@ -17,13 +17,13 @@ output "region" {
   }
 
 output "vpc" {
-  value = 
+  value = aws_vpc.aws-vpc.id
   }
 
 output "subnets" {
-  value = 
+  value = aws_subnet_aws-subnet.*.id
   }
 
 output "security_groups" {
-  value = 
+  value = aws_security_group.ecs-cluster-host.name
   }
