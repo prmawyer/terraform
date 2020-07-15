edb29207c3ff2d8b3291422c00cda198c812df8f
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-07-15 07:53:14 -0400

    Update output.tf

--- ecs-example/output.tf
+++ ecs-example/output.tf
@@ -1,9 +1,3 @@
-# output ecs cluster public ip
-output "ecs_cluster_runner_ip" {
-  description = "External IP of ECS Cluster"
-  value = [aws_instance.ecs-cluster-runner.*.public_ip]
-}
-
 output "ecs_cluster_name" {
   value = var.app_name
   }
